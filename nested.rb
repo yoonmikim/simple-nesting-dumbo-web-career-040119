9fd84f83c3a07fa720da1035a47f56bc8989d726
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 09:57:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,6 +87,7 @@ programmer_hash[:yukihiro_matsumoto][:languages] = []
 programmer_hash[:yukihiro_matsumoto][:languages].push("LISP")
 programmer_hash[:yukihiro_matsumoto][:languages] << "C"
     
+   programmer_hash
 end
 
 def changing_alan
