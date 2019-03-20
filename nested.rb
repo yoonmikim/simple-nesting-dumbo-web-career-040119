c27c99bdcf3883cedaa8097001128b06694ebc2a
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 09:59:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -79,8 +81,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = []
+programmer_hash[:yukihiro_matsumoto][:languages].push("LISP")
+programmer_hash[:yukihiro_matsumoto][:languages] << "C"
     
+   programmer_hash
 end
 
 def changing_alan
@@ -102,7 +109,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
+  
 end
 
 def adding_to_dennis
@@ -121,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << 'Assembly' 
+  programmer_hash
 end
