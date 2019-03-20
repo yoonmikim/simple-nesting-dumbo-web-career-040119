8c7163b8f5243b8b005ce10248ac2723c64d6cc5
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 09:47:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {}
+#  => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
     
 end
 
